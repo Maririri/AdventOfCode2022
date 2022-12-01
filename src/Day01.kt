f91b1e6f16fdd4d79bd37bb85f46e5f7bd882891
Mari Baburina <maria.baburina@jetbrains.com>
2022-12-01 09:42:08 +0100

    Day1 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,13 +4,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { s -> s.split("\n").sumOf { it.toInt() } }.sortedDescending().slice(0..2).sum()
     }
 
     val testInput = readInputSplintByBlankLines("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInputSplintByBlankLines("Day01")
+    val input2 = readInputSplintByBlankLines("Day02")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input2))
 }
