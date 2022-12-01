f202ee25c941de11ebcd7a88c2cb46870beb9255
Mari Baburina <maria.baburina@jetbrains.com>
2022-12-01 09:20:25 +0100

    Day1 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.maxOf { s -> s.split("\n").sumOf { it.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputSplintByBlankLines("Day01_test")
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = readInputSplintByBlankLines("Day01")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
